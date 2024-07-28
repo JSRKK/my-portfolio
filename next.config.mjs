799adf0c1617ac8ea0c380f018b312febce4aaf5
JSRKK <seera.kaenkaew8@gmail.com>
2024-07-28 12:16:00 +0700

    fix: Updated `next.config.mjs` to dynamically set `basePath` and `assetPrefix` based on the environment.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  basePath: "/my-portfolio",
+  basePath: isProd ? "/my-portfolio" : "",
+  assetPrefix: isProd ? "/my-portfolio" : "",
   output: "export",
 };
 
